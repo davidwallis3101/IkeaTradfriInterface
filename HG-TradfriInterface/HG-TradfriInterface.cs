38666ba41bed4f778420a18ccc9502da9fe76ecf
David Wallis <david@wallis2000.co.uk>
2018-01-23 16:25:23 +0000

    Initial commit after fixing references

---
+++ HG-TradfriInterface/HG-TradfriInterface.cs
@@ -0,0 +1,291 @@
+﻿// <copyright file="TradfriInterface.cs" company="Wallis2000">
+// This file is part of the Homegenie-BE Ikea Tradfri Interface Project source code.
+//
+// Author: David Wallis david@wallis2000.co.uk
+// Project Homepage: http://github.com/davidwallis3101/TradfriInterface
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using MIG.Config;
+using NLog;
+using Tomidix.CSharpTradFriLibrary;
+using Tomidix.CSharpTradFriLibrary.Models;
+
+// Your namespace must begin MIG.Interfaces for MIG to be able to load it
+namespace MIG.Interfaces.HomeAutomation
+{
+    /// <summary>
+    /// TradfriInterface class
+    /// </summary>
+    public class TradfriInterface : MigInterface
+    {
+        /// <summary>
+        /// Logger
+        /// </summary>
+        private static readonly Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// List containing interface Modules
+        /// </summary>
+        private readonly List<InterfaceModule> modules;
+
+        private List<TradFriDevice> devices;
+
+        private TradFriCoapConnector gatewayConnection;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TradfriInterface"/> class.
+        /// </summary>
+        public TradfriInterface()
+        {
+            // this.modules = new List<InterfaceModule>();
+
+            // manually add some fake modules
+            // var module_1 = new InterfaceModule
+            // {
+            //     Domain = this.GetDomain(),
+            //     Address = "1",
+            //     ModuleType = ModuleTypes.Light
+            // };
+
+            // var module_2 = new InterfaceModule
+            // {
+            //     Domain = this.GetDomain(),
+            //     Address = "2",
+            //     ModuleType = ModuleTypes.Sensor
+            // };
+
+            // var module_3 = new InterfaceModule
+            // {
+            //     Domain = this.GetDomain(),
+            //     Address = "3",
+            //     ModuleType = ModuleTypes.Sensor
+            // };
+
+            // add them to the modules list
+            // this.modules.Add(module_1);
+            // this.modules.Add(module_2);
+            // this.modules.Add(module_3);
+        }
+
+        /// <summary>
+        /// Event for Interface Modules Changing
+        /// </summary>
+        public event InterfaceModulesChangedEventHandler InterfaceModulesChanged;
+
+        /// <summary>
+        /// Event for Interface Properties Changing
+        /// </summary>
+        public event InterfacePropertyChangedEventHandler InterfacePropertyChanged;
+
+        /// <summary>
+        /// Enum containing the commands that are permitted via the web interface
+        /// </summary>
+        public enum Commands
+        {
+            /// <summary>
+            /// No Set Command Example
+            /// </summary>
+            NotSet,
+
+            /// <summary>
+            /// Control.On Example
+            /// </summary>
+            Control_On,
+
+            /// <summary>
+            /// Control.Off Example
+            /// </summary>
+            Control_Off,
+
+            /// <summary>
+            /// Temperature.Get Example
+            /// </summary>
+            Temperature_Get,
+
+            /// <summary>
+            /// Greet.Hello
+            /// </summary>
+            Greet_Hello
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this interface is enabled
+        /// </summary>
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets a the interface options
+        /// </summary>
+        public List<Config.Option> Options { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the interface/controller device is connected or not.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if it is connected; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsConnected { get; private set; }
+
+        /// <summary>
+        /// Called when disposing the Mig Interface
+        /// </summary>
+        public void Dispose()
+        {
+            this.Disconnect();
+        }
+
+        /// <summary>
+        /// Called when setting an interface option?
+        /// </summary>
+        /// <param name="option">The interface option</param>
+        public void OnSetOption(Config.Option option)
+        {
+            if (this.IsEnabled)
+            {
+                this.Connect();
+            }
+        }
+
+        /// <summary>
+        /// Gets the interface modules
+        /// </summary>
+        /// <returns>A list containing the interfaces modules</returns>
+        public List<InterfaceModule> GetModules()
+        {
+            return this.modules;
+        }
+
+        /// <summary>
+        /// Returns true if the device has been found in the system
+        /// </summary>
+        /// <returns>Boolean value indicating if the device is present</returns>
+        public bool IsDevicePresent()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Sample Connect Method
+        /// </summary>
+        /// <returns>boolean value indicating if the connection was successful</returns>
+        public bool Connect()
+        {
+            if (!this.IsConnected)
+            {
+                // Log that the interface is loading and display the version being loaded
+                Log.Info("Starting Ikea Tradfri Interface, Version {0}", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+                this.devices = new List<TradFriDevice>();
+
+                if (string.IsNullOrEmpty(this.GetOption("GatewayName").Value)) { throw new Exception("Gateway name not configured"); }
+                if (string.IsNullOrEmpty(this.GetOption("GatewayAddress").Value)) { throw new Exception("Gateway address not configured"); }
+                if (string.IsNullOrEmpty(this.GetOption("GatewaySecret").Value)) { throw new Exception("Gateway secret not configured"); }
+
+                gatewayConnection = new TradFriCoapConnector(
+                    this.GetOption("GatewayName").Value,
+                    this.GetOption("GatewayAddress").Value,
+                    this.GetOption("GatewaySecret").Value);
+
+                gatewayConnection.Connect();
+                Log.Info("Connected");
+                // TODO: Perform a connection to your 'hardware' here
+                this.IsConnected = true;
+            }
+
+            this.OnInterfaceModulesChanged(this.GetDomain());
+            return true;
+        }
+
+        /// <summary>
+        /// Sample Disconnect Method
+        /// </summary>
+        public void Disconnect()
+        {
+            if (this.IsConnected)
+            {
+                // TODO: Perform a disconnection from your 'hardware' here
+                this.IsConnected = false;
+            }
+        }
+
+        /// <summary>
+        /// Handles the control of the interface from the Mig / HG web interface
+        /// </summary>
+        /// <param name="request">request body</param>
+        /// <returns>object</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Argument out of range</exception>
+        public object InterfaceControl(MigInterfaceCommand request)
+        {
+            var response = new ResponseText("OK"); // default success value
+
+            Commands command;
+            Enum.TryParse<Commands>(request.Command.Replace(".", "_"), out command);
+
+            var module = this.modules.Find(m => m.Address.Equals(request.Address));
+
+            if (module != null)
+            {
+                switch (command)
+                {
+                    case Commands.Control_On:
+                        this.OnInterfacePropertyChanged(this.GetDomain(), request.Address, "Test Interface", "Status.Level", 1);
+                        break;
+                    case Commands.Control_Off:
+                        this.OnInterfacePropertyChanged(this.GetDomain(), request.Address, "Test Interface", "Status.Level", 0);
+                        break;
+                    case Commands.Temperature_Get:
+                        this.OnInterfacePropertyChanged(this.GetDomain(), request.Address, "Test Interface", "Sensor.Temperature", 19.75);
+                        break;
+                    case Commands.Greet_Hello:
+                        this.OnInterfacePropertyChanged(this.GetDomain(), request.Address, "Test Interface", "Sensor.Message", string.Format("Hello {0}", request.GetOption(0)));
+                        response = new ResponseText("Hello World!");
+                        break;
+                    case Commands.NotSet:
+                        break;
+                    default:
+                        Log.Error(new ArgumentOutOfRangeException(), "Command [{0}] not recognised", command);
+                        break;
+                }
+            }
+            else
+            {
+                response = new ResponseText("ERROR: invalid module address");
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Called when the interface mdoules have changed
+        /// </summary>
+        /// <param name="domain">The domain</param>
+        protected virtual void OnInterfaceModulesChanged(string domain)
+        {
+            if (this.InterfaceModulesChanged != null)
+            {
+                var args = new InterfaceModulesChangedEventArgs(domain);
+                this.InterfaceModulesChanged(this, args);
+            }
+        }
+
+        /// <summary>
+        /// Call this when an interface property has changed to notify MIG
+        /// </summary>
+        /// <param name="domain">Domain</param>
+        /// <param name="source">Source</param>
+        /// <param name="description">Description</param>
+        /// <param name="propertyPath">Property Path</param>
+        /// <param name="propertyValue">Property Value</param>
+        protected virtual void OnInterfacePropertyChanged(string domain, string source, string description, string propertyPath, object propertyValue)
+        {
+            if (this.InterfacePropertyChanged != null)
+            {
+                var args = new InterfacePropertyChangedEventArgs(domain, source, description, propertyPath, propertyValue);
+                this.InterfacePropertyChanged(this, args);
+            }
+        }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,92 @@
+﻿// <copyright file="Program.cs" company="GenieLabs">
+// This file is part of HomeGenie Project source code.
+//
+// HomeGenie is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// HomeGenie is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with HomeGenie.  If not, see http://www.gnu.org/licenses.
+//
+// Author: Generoso Martello gene@homegenie.it
+// Project Homepage: http://homegenie.it
+// </copyright>
+
+using System;
+using System.IO;
+using System.Xml.Serialization;
+using MIG;
+using MIG.Config;
+
+namespace TestProject
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main Program
+        /// </summary>
+        /// <param name="args">command line arguments</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Mig Interface test APP");
+
+            var migService = new MigService();
+
+            // Load the configuration from systemconfig.xml file
+            MigServiceConfiguration configuration;
+
+            // Construct an instance of the XmlSerializer with the type
+            // of object that is being deserialized.
+            XmlSerializer mySerializer = new XmlSerializer(typeof(MigServiceConfiguration));
+
+            // To read the file, create a FileStream.
+            FileStream myFileStream = new FileStream("systemconfig.xml", FileMode.Open);
+
+            // Call the Deserialize method and cast to the object type.
+            configuration = (MigServiceConfiguration)mySerializer.Deserialize(myFileStream);
+
+            // Set the configuration and start MIG Service
+            migService.Configuration = configuration;
+            migService.StartService();
+
+            // Get a reference to the test interface
+            var interfaceDomain = "HomeAutomation.TradfriInterface";
+            var migInterface = migService.GetInterface(interfaceDomain);
+
+            migInterface.Connect();
+
+            // Test an interface API command programmatically <module_domain>/<module_address>/<command>[/<option_0>[/../<option_n>]]
+            // var response = migInterface.InterfaceControl(new MigInterfaceCommand(interfaceDomain + "/3/Greet.Hello/Username"));
+
+            // <module_domain> ::= "Example.InterfaceSkelton"
+            // <module_address> ::= "3"
+            // <command> ::= "Greet.Hello"
+            // <option_0> ::= "Username"
+            // For more infos about MIG API see:
+            //    http://genielabs.github.io/HomeGenie/api/mig/overview.html
+            //    http://genielabs.github.io/HomeGenie/api/mig/mig_api_interfaces.html
+
+            // The same command can be invoked though the WebGateway
+            // http://<server_address>:8080/api/Example.InterfaceSkelton/1/Greet.Hello/Username
+
+            // Test some other interface API command
+            // response = migInterface.InterfaceControl(new MigInterfaceCommand(interfaceDomain + "/1/Control.On"));
+            // MigService.Log.Debug(response);
+            // response = migInterface.InterfaceControl(new MigInterfaceCommand(interfaceDomain + "/1/Control.Off"));
+            // MigService.Log.Debug(response);
+            // response = migInterface.InterfaceControl(new MigInterfaceCommand(interfaceDomain + "/2/Temperature.Get"));
+            // MigService.Log.Debug(response);
+
+            Console.WriteLine("\n[Press Enter to Quit]\n");
+            Console.ReadLine();
+        }
+    }
+}
